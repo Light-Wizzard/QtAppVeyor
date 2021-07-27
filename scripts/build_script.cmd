82d45231befcb1381c64237bb645049802c2e04c
Light-Wizzard <thelightwizzard@gmail.com>
2021-07-27 16:21:49 -0700

    Update

--- scripts/build_script.cmd
+++ scripts/build_script.cmd
@@ -5,13 +5,13 @@ mkdir build
 cd build 
 mkdir AppDir 
 if %PLATFORM%==x86
-{
+(
     qmake -v && qmake ..\%BIN_PRO_RES_NAME%.pro CONFIG+=%configuration% CONFIG+=c++11 DESTDIR=%cd% && mingw32-make && mingw32-make install INSTALL_ROOT=AppDir
-}
+)
 if %PLATFORM%==x64
-{
+(
     qmake -v && qmake ..\%BIN_PRO_RES_NAME%.pro CONFIG+=%configuration% CONFIG+=c++11 DESTDIR=%cd% && mingw32-make && mingw32-make install INSTALL_ROOT=AppDir
-}
+)
 echo "after_build Windows"
 dir 
 
@@ -21,25 +21,25 @@ windeployqt "AppDir/%BIN_PRO_RES_NAME%.exe" --verbose=2
 
 mv %BIN_PRO_RES_NAME%.exe %BIN_PRO_RES_NAME%-%PLATFORM%.exe
 
-cp -v "${APPVEYOR_BUILD_FOLDER}/desktop/${BIN_PRO_RES_NAME}.png" "${APPVEYOR_BUILD_FOLDER}/${QIF_PACKAGE_URI}/data";
-cp -v "${APPVEYOR_BUILD_FOLDER}/desktop/${BIN_PRO_RES_NAME}.svg" "${APPVEYOR_BUILD_FOLDER}/${QIF_PACKAGE_URI}/data";
-cp -v "${APPVEYOR_BUILD_FOLDER}/desktop/${BIN_PRO_RES_NAME}.ico" "${APPVEYOR_BUILD_FOLDER}/${QIF_PACKAGE_URI}/data";
-rsync -Ravr "${APPVEYOR_BUILD_FOLDER}/usr/share/icons" "${APPVEYOR_BUILD_FOLDER}/${QIF_PACKAGE_URI}/icons";
+cp -v "%APPVEYOR_BUILD_FOLDER%/desktop/%BIN_PRO_RES_NAME%.png" "%APPVEYOR_BUILD_FOLDER%/%QIF_PACKAGE_URI%/data";
+cp -v "%APPVEYOR_BUILD_FOLDER%/desktop/%BIN_PRO_RES_NAME%.svg" "%APPVEYOR_BUILD_FOLDER%/%QIF_PACKAGE_URI%/data";
+cp -v "%APPVEYOR_BUILD_FOLDER%/desktop/%BIN_PRO_RES_NAME%.ico" "%APPVEYOR_BUILD_FOLDER%/%QIF_PACKAGE_URI%/data";
+rsync -Ravr "%APPVEYOR_BUILD_FOLDER%/usr/share/icons" "%APPVEYOR_BUILD_FOLDER%/%QIF_PACKAGE_URI%/icons";
 
-ls "${APPVEYOR_BUILD_FOLDER}/${QIF_PACKAGE_URI}/data";
+ls "%APPVEYOR_BUILD_FOLDER%/%QIF_PACKAGE_URI%/data";
 
 echo "Running Qt Installer Framework";
 
 if %PLATFORM%==x64
-{
-    export ARTIFACT_APPIMAGE="${BIN_PRO_RES_NAME}-x64.exe";
-}
+(
+    export ARTIFACT_APPIMAGE="%BIN_PRO_RES_NAME%-x64.exe";
+)
 else
-{
-    export ARTIFACT_APPIMAGE="${BIN_PRO_RES_NAME}-x86.exe";
-}
+(
+    export ARTIFACT_APPIMAGE="%BIN_PRO_RES_NAME%-x86.exe";
+)
 
-export ARTIFACT_QIF="${BIN_PRO_RES_NAME}-Linux-Installer";
+export ARTIFACT_QIF="%BIN_PRO_RES_NAME%-Linux-Installer";
 
 7z a -tzip "%BIN_PRO_RES_NAME%-%MY_OS%-%CONFIGURATION%-%PLATFORM%.zip" AppDir -r
 copy "%APPVEYOR_BUILD_FOLDER%\build\%BIN_PRO_RES_NAME%-%MY_OS%-%CONFIGURATION%-%PLATFORM%.zip" "%APPVEYOR_BUILD_FOLDER%\
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 # Qt AppVeyor
 #
-# Last Update: 21 July 2021
+# Last Update: 27 July 2021
 #
 # shell?check -x scripts/install.sh
 #
-echo install Unix
+echo "install Unix"
 #
 if [[ "$APPVEYOR_BUILD_WORKER_IMAGE" = "Ubuntu" ]]; then
+#
+    #wget -c -nv "https://download.qt.io/official_releases/qt-installer-framework/$MY_QT_IF_VERSION/QtInstallerFramework-linux-x64-$MY_QT_IF_VERSION.run" -O scripts/QtInstallerFramework-linux.run
+    curl -fsS -o scripts/QtInstallerFramework-linux.run "https://download.qt.io/official_releases/qt-installer-framework/$MY_QT_IF_VERSION/QtInstallerFramework-linux-x64-$MY_QT_IF_VERSION.run";
+    if [ -f scripts/QtInstallerFramework-linux.run ]; then
+        # make it executable
+        chmod +x scripts/QtInstallerFramework-linux.run;
+        ls -las scripts/;
+    else
+        echo -e "Error downloading QtInstallerFramework-linux";
+    fi
     # Python
     sudo add-apt-repository ppa:deadsnakes/ppa -y;
     sudo apt-get update -qq && sudo apt-get install -qq;
-    if [[ "$UPGRADE_OS" = "true" ]]; then sudo apt-get upgrade -qqy; fi
+    if [[ "$MY_UPGRADE_OS" = "true" ]]; then sudo apt-get upgrade -qqy; fi
     # Developer tools and libsqlite3
     sudo apt install -qqy build-essential cmake software-properties-common libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev;
     # common
@@ -20,20 +30,11 @@ if [[ "$APPVEYOR_BUILD_WORKER_IMAGE" = "Ubuntu" ]]; then
     if [[ "$MY_PYTHON_REQUIRED" = "true" ]]; then sudo apt-get install -qqy python3.9-dev python3-venv; fi
     # Required by LinuxDeploy
     sudo apt-get install -qqy libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-render-util0 libxcb-xinerama0 libxcb-xkb-dev libxkbcommon-x11-0 libgtk2.0-dev;
-    #
-    #wget -c -nv https://download.qt.io/official_releases/qt-installer-framework/$MY_QT_IF_VERSION/QtInstallerFramework-linux-x64-$MY_QT_IF_VERSION.run -O scripts/QtInstallerFramework-linux.run
-    curl -fsS -o scripts/QtInstallerFramework-linux.run https://download.qt.io/official_releases/qt-installer-framework/$MY_QT_IF_VERSION/QtInstallerFramework-linux-x64-$MY_QT_IF_VERSION.run
-    if [ -f scripts/QtInstallerFramework-linux.run ]; then
-        # make it executable
-        chmod +x scripts/QtInstallerFramework-linux.run;
-    else
-        echo -e "Error downloading QtInstallerFramework-linux";
-    fi
 fi
 #
 if [[ "$APPVEYOR_BUILD_WORKER_IMAGE" = "macos" ]]; then
     #
-    wget -c -nv https://download.qt.io/official_releases/qt-installer-framework/$MY_QT_IF_VERSION/QtInstallerFramework-macOS-x86_64-$MY_QT_IF_VERSION.dmg -O scripts/QtInstallerFramework-macOS.dmg
+    wget -c -nv "https://download.qt.io/official_releases/qt-installer-framework/$MY_QT_IF_VERSION/QtInstallerFramework-macOS-x86_64-$MY_QT_IF_VERSION.dmg" -O scripts/QtInstallerFramework-macOS.dmg
     # make it executable
     chmod +x scripts/QtInstallerFramework-macOS.dmg;
 fi
