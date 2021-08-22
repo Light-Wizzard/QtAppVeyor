8fca9e8a839861dae1dbaa6ae4d994eb199aedca
Light-Wizzard <thelightwizzard@gmail.com>
2021-08-22 15:34:48 -0700

    Update

--- scripts/build_script.ps1
+++ scripts/build_script.ps1
@@ -15,7 +15,7 @@ If ($env:PLATFORM -eq "x64" -And $env:MY_COMPILER -eq "Qt") {
     $env:CMAKE_PATH_PREFIX = "C:\Qt\$env:MY_QT_VERSION\msvc$env:MY_VS_VERSION_64\lib\cmake"
     $env:MY_MAKE = "cmake -A x64 -G $env:CMAKEGENERATOR -DCMAKE_PREFIX_PATH=$env:CMAKE_PATH_PREFIX -DCMAKE_BUILD_TYPE=$env:CONFIGURATION -DCMAKE_INSTALL_PREFIX=AppDir .."
     Write-Host "qmake dbug: $env:MY_MAKE"
-    cmd /c cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_PREFIX_PATH="$env:CMAKE_PATH_PREFIX" -DCMAKE_BUILD_TYPE=$env:CONFIGURATION -DCMAKE_INSTALL_PREFIX=AppDir -DCMAKE_C_COMPILER=/c/MinGW/bin/gcc.exe -DCMAKE_CXX_COMPILER=/c/MinGW/bin/g++.exe ..
+    cmd /c cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_PREFIX_PATH="$env:CMAKE_PATH_PREFIX" -DCMAKE_INSTALL_PREFIX=AppDir -DCMAKE_C_COMPILER=/c/MinGW/bin/gcc.exe -DCMAKE_CXX_COMPILER=/c/MinGW/bin/g++.exe ..
     If ($?) {
         Write-Host "build_script Windows QT x64 cmake"
         Invoke-Expression "cmake --build . --config $env:CONFIGURATION --target install"
